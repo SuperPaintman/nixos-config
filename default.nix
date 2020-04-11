3f8ddee5fb05ffd2a37caa49a72adc5a86319142
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2020-04-12 01:59:07 +0300

    feat(*): add default NixOS configuration

---
+++ default.nix
@@ -0,0 +1,76 @@
+{ pkgs, lib, options, ... }:
+{
+  # Boot.
+  boot.cleanTmpDir = true; # Delete all files in `/tmp` during boot.
+
+  # Nix Packages.
+  nixpkgs.config.allowUnfree = true;
+
+  # Networking.
+  networking.hostName = "sequoia"; # TODO(SuperPaintman): change it. Sequoia is my desktop.
+  networking.networkmanager.enable = true;
+
+  # Environment.
+  environment.systemPackages = with pkgs; [
+    # Basic packages.
+    coreutils
+    wget
+    curl
+    atop
+    htop
+    git
+    jq
+    tmux
+    xclip
+
+    # Shells.
+    bash
+    fish
+    zsh
+
+    # Editors.
+    vim
+    vscode
+
+    # Browsers.
+    firefox
+
+    # Terminals.
+    xst
+
+    # Compilers and interpreters.
+    go
+  ];
+
+  # Time.
+  time.timeZone = "Europe/Moscow";
+
+  # Sound.
+  sound.enable = true;
+
+  # Hardware.
+  hardware = {
+    pulseaudio.enable = true; # Enable the PulseAudio sound server.
+
+    opengl.enable = true; # TODO(SuperPaintman): enable it only on Sequoia.
+  };
+
+  # Services.
+  services.xserver = {
+    enable = true;
+
+    displayManager.sddm.enable = true;
+
+    desktopManager.xterm.enable = false;
+    desktopManager.plasma5.enable = true;
+
+    videoDrivers = [ "nvidia" ]; # TODO(SuperPaintman): enable it only on Sequoia.
+  };
+  
+  # Users.
+  users.users.superpaintman = {
+    isNormalUser = true;
+    uid = 1000;
+    extraGroups = [ "wheel" ];
+  };
+}
