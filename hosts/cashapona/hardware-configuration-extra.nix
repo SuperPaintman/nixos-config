7c8e021928a4c14622a04e877a08b016f703fa32
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2020-12-15 03:22:10 +0300

    fix(sequoia): fix `hardware-configuration-extra.nix`

--- hosts/cashapona/hardware-configuration-extra.nix
+++ hosts/cashapona/hardware-configuration-extra.nix
@@ -6,7 +6,7 @@
   # Boot.
   boot.loader = {
     # Use the systemd-boot EFI boot loader.
-    boot.loader.systemd-boot.enable = true;
+    systemd-boot.enable = true;
 
     efi.canTouchEfiVariables = true;
 
