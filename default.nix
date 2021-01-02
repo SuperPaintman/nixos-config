4d2d755bac88e6a9e3e6f367b4ce99b39331e311
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2021-01-03 00:30:13 +0300

    feat(default): update picom config

--- default.nix
+++ default.nix
@@ -322,7 +322,9 @@ in
       # Unredirect all windows if a full-screen opaque window is detected,
       # to maximize performance for full-screen windows. Known to cause
       # flickering when redirecting/unredirecting windows.
-      unredir-if-possible = true;
+      #
+      # NOTE(SuperPaintman): if true it makes glitches on the second screen.
+      unredir-if-possible = false;
 
       # GLX backend: Avoid using stencil buffer, useful if you don't have a
       # stencil buffer. Might cause incorrect opacity when rendering
@@ -330,11 +332,6 @@ in
       # with blur-background. My tests show a 15% performance boost.
       # Recommended.
       glx-no-stencil = true;
-
-      # Use X Sync fence to sync clients' draw calls, to make sure all draw
-      # calls are finished before picom starts drawing. Needed on
-      # nvidia-drivers with GLX backend for some users.
-      xrender-sync-fence = true;
     };
   };
 
